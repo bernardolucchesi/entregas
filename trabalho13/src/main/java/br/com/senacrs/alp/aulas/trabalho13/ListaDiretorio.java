a12d5bdd6b826ffb5515ddb85bd3a3c70ad41d64
Bernardo <ber.lucchesi@gmail.com>
2012-12-02 21:47:11 -0200

    Passando os testes, falta arrumar a ordenação.

--- trabalho13/src/main/java/br/com/senacrs/alp/aulas/trabalho13/ListaDiretorio.java
+++ trabalho13/src/main/java/br/com/senacrs/alp/aulas/trabalho13/ListaDiretorio.java
@@ -1,6 +1,8 @@
 package br.com.senacrs.alp.aulas.trabalho13;
 
+import java.awt.List;
 import java.io.File;
+
 
 
 public class ListaDiretorio {
@@ -10,17 +12,15 @@ public class ListaDiretorio {
 		if(diretorio == null){
 			throw new IllegalArgumentException();
 		}
-		String[] dir = null;
-		String caminho = null;
-		String caminhoabsoluto = null;
 		
-		final String root = System.getProperty("user.dir");
-		caminhoabsoluto = root + File.separator + caminho;
+		List lista = new List();
+		String[] dir = null;
+		String caminhoabsoluto = diretorio;
 		
 		try{
 			
 			File testeArquivo = new File(caminhoabsoluto);
-			if(!testeArquivo.isDirectory() || !testeArquivo.isFile()){
+			if(!testeArquivo.isDirectory()){
 				throw new IllegalArgumentException();
 			}
 			
@@ -28,7 +28,8 @@ public class ListaDiretorio {
 			throw new IllegalArgumentException();
 		}
 		
-		escreveDir(caminhoabsoluto);
+		lista = escreveDir(caminhoabsoluto);
+		dir = lista.getItems();
 		
 		if(dir == null){
 			throw new IllegalArgumentException();
@@ -37,24 +38,59 @@ public class ListaDiretorio {
 		return dir;
 	}
 	
-	public String[] escreveDir(String path){
+	public List escreveDir(String path){
 		
-		File arq = new File(path);
-		char teste;
-		char teste1; 
+		List conteudo = new List();
+		String[] ordenado = orderList(path);
 		
-		String[] conteudo = null;
-		String[] totalarquivos = arq.list();
+		conteudo.add("list: " + path);
+		conteudo.add("total: " + ordenado.length);
 		
-		conteudo[0] = "list: " + path;
-		conteudo[1] = "total: " + totalarquivos.length;
-		
-		for(int i = 0; i < totalarquivos.length; i++){
-			teste = totalarquivos[i].charAt(0);
-			teste1 = totalarquivos[i+1].charAt(0);
+		for(int y = 0; y < ordenado.length; y++){
+			conteudo.add(ordenado[y]);
 		}
 		
 		return conteudo;
 		
+	}
+	
+	public String[] orderList(String path){
+
+		File arq = new File(path);
+		File ord = null;
+		String aux = null;
+		
+		String[] totalarquivos = arq.list();
+		
+		for(int i = totalarquivos.length; i == 1; i--){
+			if(totalarquivos[i].compareTo(totalarquivos[i-1]) < 0){
+				aux = totalarquivos[i];
+				totalarquivos[i] = totalarquivos[i-1];
+				totalarquivos[i-1] = aux;
+			}
+		}
+		
+		for(int i = totalarquivos.length; i == 1; i--){
+
+			try{
+				ord = new File(totalarquivos[i]);
+				if(ord.isDirectory()){
+					
+					totalarquivos[i] = "d " + totalarquivos[i];
+					aux = totalarquivos[i-1];
+					totalarquivos[i-1] = totalarquivos[i];
+					totalarquivos[i-1] = aux;
+					
+				}else if(ord.isFile()){
+					
+					totalarquivos[i] = "a " + totalarquivos[i];
+					
+				}
+			}catch(IllegalArgumentException e){
+				throw new IllegalArgumentException();
+			}
+		}
+	
+		return totalarquivos;
 	}
 }
