43e6ab3c5d8062e4138ef51d5dee7112957bc9d9
bernardolucchesi <ber.lucchesi@gmail.com>
2012-12-04 17:33:21 -0200

    Passando Directory e File, falta ordenação correta.

--- trabalho13/src/main/java/br/com/senacrs/alp/aulas/trabalho13/ListaDiretorio.java
+++ trabalho13/src/main/java/br/com/senacrs/alp/aulas/trabalho13/ListaDiretorio.java
@@ -18,12 +18,10 @@ public class ListaDiretorio {
 		String caminhoabsoluto = diretorio;
 		
 		try{
-			
 			File testeArquivo = new File(caminhoabsoluto);
 			if(!testeArquivo.isDirectory()){
 				throw new IllegalArgumentException();
 			}
-			
 		}catch(IllegalArgumentException e){
 			throw new IllegalArgumentException();
 		}
@@ -50,6 +48,10 @@ public class ListaDiretorio {
 			conteudo.add(ordenado[y]);
 		}
 		
+		for(int i = 0; i <= ordenado.length; i++){
+			System.out.println(conteudo.getItem(i));
+		}
+		
 		return conteudo;
 		
 	}
@@ -57,40 +59,34 @@ public class ListaDiretorio {
 	public String[] orderList(String path){
 
 		File arq = new File(path);
-		File ord = null;
-		String aux = null;
-		
 		String[] totalarquivos = arq.list();
+		File ord = null;
+		String absolut = null;
+		boolean troca = false;
 		
-		for(int i = totalarquivos.length; i == 1; i--){
-			if(totalarquivos[i].compareTo(totalarquivos[i-1]) < 0){
-				aux = totalarquivos[i];
-				totalarquivos[i] = totalarquivos[i-1];
-				totalarquivos[i-1] = aux;
+		for(int i = 0; i < totalarquivos.length -1; i++){
+			
+			if(totalarquivos[i].compareTo(totalarquivos[i+1]) < 0){
+				String aux = totalarquivos[i];
+				totalarquivos[i] = totalarquivos[i+1];
+				totalarquivos[i+1] = aux;
 			}
 		}
 		
-		for(int i = totalarquivos.length; i == 1; i--){
-
-			try{
-				ord = new File(totalarquivos[i]);
+		do{
+			troca = false;
+			for(int i = 0; i < totalarquivos.length - 1; i++){
+				absolut = path + File.separator + totalarquivos[i];
+				ord = new File(absolut);
 				if(ord.isDirectory()){
-					
-					totalarquivos[i] = "d " + totalarquivos[i];
-					aux = totalarquivos[i-1];
-					totalarquivos[i-1] = totalarquivos[i];
-					totalarquivos[i-1] = aux;
-					
-				}else if(ord.isFile()){
-					
-					totalarquivos[i] = "a " + totalarquivos[i];
-					
+					String aux = totalarquivos[i];
+					totalarquivos[i] = totalarquivos[i+1];
+					totalarquivos[i+1] = aux;
+					//troca = true;
 				}
-			}catch(IllegalArgumentException e){
-				throw new IllegalArgumentException();
 			}
-		}
-	
+		}while(troca);
+		
 		return totalarquivos;
 	}
 }
